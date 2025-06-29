6a63efce2d5b398dd91b0e17ecb69ebd9a1d998a
singharti <artisingh3106@gmail.com>
2025-06-29 17:58:09 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -3,22 +3,41 @@ const express = require("express");
 const app = express();
 
 
-const { adminAuth , userAuth } = require("./middlewares/auth");
+// const { adminAuth , userAuth } = require("./middlewares/auth");
 
-app.use('/admin', adminAuth);
+// app.use('/admin', adminAuth);
 
 
 
 //route handler
-app.get("/admin/getData", 
+// app.get("/admin/getData", 
+//     (req, res,next) =>{
+//     res.send("Hello from the serve!")
+// });
+app.use("/",(err, req, res, next) => {
+    if(err){
+        res.status(500).send("error");
+    }
+})
+app.get("/getUserData",
     (req, res,next) =>{
-    res.send("Hello from the serve!")
+        // try{
+        throw new Error("dfdsfsd");
+
+        // }catch(err){
+        //     res.status(500).send("contact team")
+        // }
+        
+    res.send("Hello from the reponse 1!")
 });
-app.get("/user", userAuth ,
-    (req, res,next) =>{
-    // res.send("Hello from the reponse 1!")
-    next();
-});
+
+app.use("/",(err, req, res, next) => {
+    if(err){
+        res.status(500).send("error");
+    }
+})
+
+
 
 
 app.listen(3000, () => {
